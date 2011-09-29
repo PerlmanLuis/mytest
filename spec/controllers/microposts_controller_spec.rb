b2ec2378155602994336b8e627cc4c9314220c9d
Perlman Luis <luisweigh@gmail.com>
2011-09-29 20:05:30 +0800

    Finished microposts

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -29,12 +29,12 @@ describe MicropostsController do
       
       it "should not create a micropost" do
         lambda do
-          post :create, :microposts => @attr
+          post :create, :micropost => @attr
         end.should_not change(Micropost, :count)
       end
       
       it "should re-render the home page" do
-        post :create, :microposts => @attr
+        post :create, :micropost => @attr
         response.should render_template('pages/home')
       end
     end
@@ -47,18 +47,52 @@ describe MicropostsController do
       
       it "should create a micropost" do
         lambda do
-          post :create, :microposts => @attr
+          post :create, :micropost => @attr
         end.should change(Micropost, :count).by(1)
       end
       
       it "should redirect to root path" do
-        post :create, :microposts => @attr
+        post :create, :micropost => @attr
         response.should redirect_to root_path
       end
       
       it "should have a flash success message" do
-        post :create, :microposts => @attr
+        post :create, :micropost => @attr
         flash[:success].should =~ /micropost created/i      
+      end
+    end
+  end
+  
+  describe "DELETE 'destroy'" do
+    
+    describe "for an unauthorized user" do
+      
+      before(:each) do
+        @user = Factory(:user)
+        wrong_user = Factory(:user, :email => Factory.next(:email) )
+        @micropost = Factory(:micropost, :user => @user)
+        test_sign_in(wrong_user)
+      end
+      
+      it "should deny access" do
+        delete :destroy, :id => @micropost
+        response.should redirect_to(root_path)
+      end
+    end
+    
+    describe "for an authorized user" do
+      
+      before(:each) do
+        @user = test_sign_in(Factory(:user))
+        @micropost = Factory(:micropost, :user => @user)
+      end
+      
+      it "should destroy a micropost" do
+        lambda do
+          delete :destroy, :id => @micropost
+          flash[:success].should =~ /deleted/i
+          response.should redirect_to(root_path)
+        end.should change(Micropost, :count).by(-1)
       end
     end
   end
